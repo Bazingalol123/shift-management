ca0177737fb2a76fbc463bbd5f6d989e1c96d059
Bazingalol123 <50514170+Bazingalol123@users.noreply.github.com>
2025-04-01 13:29:31 +0000

    Deploying to main from @ Bazingalol123/shift-management@276268eacbd2a57c1bfc0c43f5b280d96e659c2e 🚀

--- github-config.js
+++ github-config.js
@@ -1,6 +1,7 @@
+// Auto-generated file - DO NOT EDIT
 const GITHUB_CONFIG = {
-  TOKEN: "your-development-token",
-  OWNER: "your-username",
-  REPO: "shift-management",
+  TOKEN: "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",
+  OWNER: "Bazingalol123",
+  REPO: "",
   BRANCH: "main"
 };
